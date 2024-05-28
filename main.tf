e3dec7a43e7e8f0756ad2056b9a244acc4a0815d
Vishal Mudgal <84653428+vishal0223@users.noreply.github.com>
2024-05-28 17:28:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "FirstTfEc2" {
   key_name                    = "my-first-keypair"
   associate_public_ip_address = true
   tags = {
-    Name = "Firstec2-11"
+    Name = "Firstec2"
   }
 
 }
