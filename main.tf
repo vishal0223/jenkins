67e104eb7e9fe001c0bb01b9e57059ccdfc505c3
Vishal <vishalmudgal2302@gmail.com>
2024-05-28 17:17:02 +0530

    jenkins file updated

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-
+provider "aws" {
+  region = "ap-south-1"
+}
 resource "aws_instance" "FirstTfEc2" {
   ami                         = "ami-0cc9838aa7ab1dce7"
   instance_type               = "t2.micro"
